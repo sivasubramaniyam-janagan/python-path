27e0d0aaf447fc8883497685946f03153032942d
sivasubramaniyam-janagan <sivasubramaniyamjanagan@gmail.com>
2026-05-13 04:52:11 +0530

    Add files via upload

---
+++ numpy/importing.py
@@ -0,0 +1,20 @@
+import numpy as  np
+array=np.array([1,2,3,4,5,6,7,8])
+print(array*2)
+
+print("2d arrays.........")
+
+array2=np.array([[1,2,3],
+                             [4,5,6],
+                             [7,8,9]
+                            ])
+
+print(array2[0][1])
+print(array2[1,2])
+
+
+
+
+
+
+
---
+++ numpy/slicing.py
@@ -0,0 +1,6 @@
+import numpy as np
+array=np.array([[1,3,4],
+                                 [23,4,5],
+                                 [34,23,45]])
+
+print(array[0:2])
